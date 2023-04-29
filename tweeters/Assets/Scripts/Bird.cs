94de9c04f96308340d91ae31b0cf550526548e61
DaeStan <stanifed@my.erau.edu>
2023-04-29 13:14:33 -0700

    Implemented DragLine

--- tweeters/Assets/Scripts/Bird.cs
+++ tweeters/Assets/Scripts/Bird.cs
@@ -11,6 +11,8 @@ public class Bird : MonoBehaviour
     Rigidbody2D _rigidbody2D;
     SpriteRenderer _spriteRenderer;
 
+    public bool IsDragging {get; private set;}
+
     void Awake()
     {
         _rigidbody2D = GetComponent<Rigidbody2D>();
@@ -27,6 +29,7 @@ public class Bird : MonoBehaviour
     void OnMouseDown()
     {
         _spriteRenderer.color = Color.red;
+        IsDragging = true;
     }
 
     void OnMouseUp()
@@ -39,6 +42,7 @@ public class Bird : MonoBehaviour
         _rigidbody2D.AddForce(direction * _launchForce);
 
         _spriteRenderer.color = Color.white;
+        IsDragging = false;
     }
 
     void OnMouseDrag()
@@ -63,7 +67,7 @@ public class Bird : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     void OnCollisionEnter2D(Collision2D collision)
